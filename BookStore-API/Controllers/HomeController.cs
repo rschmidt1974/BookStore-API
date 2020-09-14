072b838876e144b060ccb28dae839329a3c5dbc2
rschmidt1974@outlook.com <rschmidt1974@outlook.com>
2020-09-13 19:39:45 -0500

    added database and context

---
+++ BookStore-API/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore_API.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILoggerService _logger;
+
+        public HomeController(ILoggerService logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            _logger.LogInfo("Accessed Home Controller");
+
+            return Ok("Hello World");
+        }
+    }
+}
